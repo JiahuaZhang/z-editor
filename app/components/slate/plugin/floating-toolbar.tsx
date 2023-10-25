867983dc86141510bf5b308360f06385d3afa3b8
Jiahua Zhang <hughzhanghz@gmail.com>
2023-10-25 17:51:35 -0400

    re-init floating toolbar using iconify

--- app/components/slate/plugin/floating-toolbar.tsx
+++ app/components/slate/plugin/floating-toolbar.tsx
@@ -1,10 +1,28 @@
 import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
-import { BsTypeBold } from 'react-icons/bs/index.js';
 
 export const FloatingToolbar = () => {
   const [isClient, setIsClient] = useState(false);
-  const content = <BsTypeBold />;
+  const content = <div
+    className=''
+    un-position='absolute'
+    un-top='0'
+    un-bg='gray-7'
+    un-border='rounded'
+    un-font='1.5em'
+    un-text='2xl'
+    un-size='1.5em'
+    un-flex='~'
+    un-p='1'
+  >
+    <div className='i-radix-icons:font-bold'
+      un-color='white'
+    />
+    <div className='i-radix-icons:font-italic'
+      un-color='white' />
+    <div className='i-tabler:underline'
+      un-color='white' />
+  </div>;
 
   useEffect(() => setIsClient(true), []);
 
--- unocss.config.js
+++ unocss.config.js
@@ -1,5 +1,9 @@
 import { defineConfig, presetUno, presetIcons, presetAttributify, presetTypography } from 'unocss'
 
 export default defineConfig({
-  presets: [presetUno(), presetAttributify(), presetIcons(), presetTypography()],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({ extraProperties: { display: 'inline-block' } }),
+    presetTypography()],
 })

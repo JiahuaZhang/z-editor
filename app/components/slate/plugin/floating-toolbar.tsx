b34436791752dc570e0d4ec1036714fade353b0b
Jiahua Zhang <hughzhanghz@gmail.com>
2023-10-29 20:35:14 -0400

    update css for floating toolbar

--- app/components/slate/plugin/floating-toolbar.tsx
+++ app/components/slate/plugin/floating-toolbar.tsx
@@ -1,18 +1,58 @@
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { ClientOnly } from "remix-utils/client-only";
+import { Editor, Range } from 'slate';
+import { useFocused, useSlate } from 'slate-react';
 
 export const FloatingToolbar = () => {
-  const content = <div
-    className=''
+  const ref = useRef<HTMLDivElement>(null);
+  const editor = useSlate();
+  const isFocus = useFocused();
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    const { selection } = editor;
+    if (
+      !selection ||
+      !isFocus ||
+      Range.isCollapsed(selection) ||
+      Editor.string(editor, selection) === ''
+    ) {
+      element.removeAttribute('style');
+      return;
+    }
+
+    const domSelection = window.getSelection()!;
+    const domRange = domSelection.getRangeAt(0);
+    const rect = domRange.getBoundingClientRect();
+
+    if (rect.top < window.innerHeight / 2) {
+      element.style.top = `${rect.top + window.scrollY + rect.height + 4}px`;
+    } else {
+      element.style.top = `${rect.top + window.scrollY - element.offsetHeight - 4}px`;
+    }
+
+    const left = rect.left + window.scrollX - element.offsetWidth / 2 + rect.width / 2;
+    if (left + element.offsetWidth > window.innerWidth) {
+      element.style.right = '1em';
+    } else {
+      element.style.left = `${left}px`;
+    }
+    element.style.opacity = '1';
+  });
+
+  const Toolbar = () => <div
+    ref={ref}
     un-position='absolute'
-    un-top='0'
     un-bg='gray-7'
     un-border='rounded'
-    un-font='1.5em'
     un-text='2xl'
-    un-size='1.5em'
     un-flex='~'
-    un-p='1'
+    un-p='x-2 y-1'
+    un-opacity='0'
+    un-transition='opacity 200'
   >
     <div className='i-radix-icons:font-bold'
       un-color='white'
@@ -24,6 +64,6 @@ export const FloatingToolbar = () => {
   </div>;
 
   return <ClientOnly>
-    {() => createPortal(content, document.body)}
+    {() => createPortal(<Toolbar />, document.body)}
   </ClientOnly>;
 };

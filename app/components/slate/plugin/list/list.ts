4f18aecd1b0332375cc03db489450bfa31e9003d
Jiahua Zhang <hughzhanghz@gmail.com>
2024-01-16 23:06:01 -0500

    update list indentation, outdentation logic

--- app/components/slate/plugin/list/list.ts
+++ app/components/slate/plugin/list/list.ts
@@ -46,175 +46,105 @@ const listIndentation = (event: React.KeyboardEvent, editor: Editor) => {
   if (!match) return false;
 
   event.preventDefault();
-  const [node, path] = match;
+  const [_, path] = match;
 
   if (event.shiftKey) {
+    const parentPath = Path.parent(path);
+    if (parentPath.length === 1) return true;
 
+    const nextPath = Path.next(path);
+    try {
+      const [nextNode] = Editor.node(editor, nextPath);
+      if (['ul', 'ol'].includes((nextNode as any).type)) {
+        return true;
+      }
+    } catch (error) {}
+
+    Transforms.liftNodes(editor, { at: path });
+    return true;
   } else {
     if (path[path.length - 1] === 0) {
       return true;
     }
 
-    const prevPath = Path.previous(path);
     const [parentNode] = Editor.node(editor, Path.parent(path));
-    const [prevNode] = Editor.node(editor, prevPath);
-
-    if (!prevNode) return true;
-
-    if ((prevNode as any).type === 'li') {
-      Transforms.wrapNodes(editor, { type: (parentNode as any).type, children: [] });
-      return true;
-    }
+    Transforms.wrapNodes(editor, { type: (parentNode as any).type, children: [] });
+    return true;
   }
-
-  return false;
 };
 
 export const onKeyDownForList = (event: React.KeyboardEvent, editor: Editor) =>
   insertNewParagraph(event, editor) || listIndentation(event, editor);
 
-// export const dummyData = [{
-//   type: 'ul',
-//   children: [
-//     {
-//       type: 'li',
-//       children: [{ text: '1st item' }],
-//     },
-//     {
-//       type: 'ul',
-//       children: [
-//         {
-//           type: 'li',
-//           children: [{ text: 'A nested list item' }],
-//         },
-//         {
-//           type: 'ul',
-//           children: [
-//             {
-//               type: 'li',
-//               children: [{ text: 'A nested list item' }],
-//             },
-//             {
-//               type: 'li',
-//               children: [{ text: 'super nested list item' }],
-//             },
-//             {
-//               type: 'li',
-//               children: [{ text: 'super super nested list item' }],
-//             },
-//           ],
-//         },
-//         { type: 'li', children: [{ text: 'after nested item' }] },
-//         {
-//           type: 'li',
-//           children: [
-//             { text: 'still nested list item' },
-//             {
-//               type: 'ul',
-//               children: [{ type: 'li', children: [{ text: 'I am nested' }] }],
-//             }
-//           ],
-//         },
-//       ],
-//     },
-//     {
-//       type: 'li',
-//       children: [{ text: 'another list item' }],
-//     },
-//     {
-//       type: 'li',
-//       children: [{ text: 'still a list item' }],
-//     },
-//     {
-//       type: 'li',
-//       children: [{ text: 'random list item' }],
-//     },
-//     {
-//       type: 'li',
-//       children: [
-//         { text: 'another list item' },
-//         {
-//           type: 'ul', children: [
-//             {
-//               type: 'li',
-//               children: [{ text: 'still a list item' }], // relocated to here
-//             },
-//           ]
-//         }
-//       ],
-//     }
-//   ],
-// }];
-
-const expected = [
-  {
-    "type": "ul",
-    "children": [
-      {
-        "type": "li",
-        "children": [
-          {
-            "text": "another list item"
-          }
-        ]
-      },
-      {
-        "type": "ul",
-        "children": [
-          {
-            "type": "li",
-            "children": [
-              {
-                "text": "still a list item"
-              }
-            ]
-          },
-          {
-            "type": "li",
-            "children": [
-              {
-                "text": "random list item"
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-];
-
-const simple = [
-  {
-    "type": "ul",
-    "children": [
-      {
-        "type": "li",
-        "children": [
-          {
-            "text": "another list item"
-          }
-        ]
-      },
-      {
-        "type": "li",
-        "children": [
-          {
-            "text": "still a list item"
-          }
-        ]
-      },
-      {
-        "type": "li",
-        "children": [
-          {
-            "text": "random list item" // tab key pressed here
-          }
-        ]
-      }
-    ]
-  }
-];
-
-const dummyData = simple;
-
-export { dummyData };
+export const dummyData = [{
+  type: 'ul',
+  children: [
+    {
+      type: 'li',
+      children: [{ text: '1st item' }],
+    },
+    {
+      type: 'ul',
+      children: [
+        {
+          type: 'li',
+          children: [{ text: 'A nested list item' }],
+        },
+        {
+          type: 'ul',
+          children: [
+            {
+              type: 'li',
+              children: [{ text: 'A nested list item' }],
+            },
+            {
+              type: 'li',
+              children: [{ text: 'super nested list item' }],
+            },
+            {
+              type: 'li',
+              children: [{ text: 'super super nested list item' }],
+            },
+          ],
+        },
+        { type: 'li', children: [{ text: 'after nested item' }] },
+        {
+          type: 'li',
+          children: [
+            { text: 'still nested list item' },
+          ],
+        },
+        {
+          type: 'ul',
+          children: [{ type: 'li', children: [{ text: 'I am nested' }] }],
+        }
+      ],
+    },
+    {
+      type: 'li',
+      children: [{ text: 'another list item' }],
+    },
+    {
+      type: 'li',
+      children: [{ text: 'still a list item' }],
+    },
+    {
+      type: 'li',
+      children: [{ text: 'random list item' }],
+    },
+    {
+      type: 'li',
+      children: [
+        { text: 'another list item' },
+      ],
+    },
+    {
+      type: 'ul', children: [
+        {
+          type: 'li',
+          children: [{ text: 'still a list item' }], // relocated to here
+        },
+      ]
+    }
+  ],
+}];

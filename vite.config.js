00619108826ed26b032ddfb5fb7fafb72f0c38e7
Jiahua Zhang <hughzhanghz@gmail.com>
2024-02-21 17:33:18 -0500

    upgrade remix 2.7

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
-import { unstable_vitePlugin as remix } from '@remix-run/dev'
+import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-installGlobals()
+// installGlobals();
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~': `${process.cwd()}/app`
-    }
-  },
-  server: {
-    port: 3000,
-  },
+  // server: {
+  //   port: 3000,
+  // },
   plugins: [
     UnoCSS(),
     remix({
       ignoredRouteFiles: ['**/.*']
-    })
+    }),
+    tsconfigPaths()
   ]
 })

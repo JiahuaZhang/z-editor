a8149e8d35ae352f637363c0e9aa733e23bd85b4
Jiahua Zhang <hughzhanghz@gmail.com>
2024-04-05 19:46:30 -0400

    update type.d.ts

--- app/lib/structure/type.d.ts
+++ app/lib/structure/type.d.ts
@@ -4,47 +4,27 @@ export type Color = NamedColor | HexColor;
 export type SimpleRichContentLabel = 'span' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'blockquote';
 export type RichContentLabel = SimpleRichContentLabel | 'ul' | 'li' | 'ol' | 'code' | 'embed' | 'image' | 'link' | 'hashTag' | 'inlinePanel' | string;
 
-// export type ContentState = {
-//   element?: HTMLElement;
-//   id?: string;
-//   path?: string;
-// } & Record<string, any>;
+type PlainData = {
+  id?: string;
+  text: string;
+};
 
-export type ContentData = ({ text: string; }
-  | {
-    value?: string;
-    bold?: boolean;
-    underline?: boolean;
-    italic?: boolean;
-    color?: Color;
-    background?: Color;
-  }) & { id?: string; } & Record<string, string>;
+type ComplexData = {
+  id?: string;
+  value?: string;
+  bold?: boolean;
+  underline?: boolean;
+  italic?: boolean;
+  color?: Color;
+  background?: Color;
+  [key: string]: string | number | boolean | undefined;
+};
+
+export type ContentData = PlainData | ComplexData;
 
 export type Content = {
   label: RichContentLabel;
-  // state?: ContentState;
   data?: ContentData;
   children?: Content[],
   id?: string;
-};
-
-// todo:
-// state & path needs to be completely separated?
-// react would be confused -> self reference issue..
-
-// Init, content => sync + content & state ==> update? => state + => sync content => react render..
-
-// array issue
-// insertion, deletion should be O(1) for rich text editor
-
-type ManagedContent = {
-  content: Content;
-  prev: ManagedContent | null;
-  next: ManagedContent | null;
-};
-
-type ContentManager = {
-  head: ManagedContent | null;
-  tail: ManagedContent | null;
-  map: Map<string, ManagedContent>;
 };

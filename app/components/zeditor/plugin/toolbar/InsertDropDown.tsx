e5f12bda8f797e1ffac284a1b186964c869d3619
Jiahua Zhang <hughzhanghz@gmail.com>
2024-12-23 09:40:11 -0500

    add insert inline image

--- app/components/zeditor/plugin/toolbar/InsertDropDown.tsx
+++ app/components/zeditor/plugin/toolbar/InsertDropDown.tsx
@@ -1,4 +1,4 @@
-import { Button, Dropdown, Form, Input, MenuProps, Modal, Upload } from 'antd';
+import { Button, Checkbox, Dropdown, Form, Input, MenuProps, Modal, Radio, Upload } from 'antd';
 import { useAtomValue } from 'jotai';
 import { $getRoot } from 'lexical';
 import { useCallback, useMemo, useState } from 'react';
@@ -7,13 +7,15 @@ import { INSERT_INLINE_COMMAND } from '../comment/CommentPlugin';
 import { INSERT_EXCALIDRAW_COMMAND } from '../excalidraw/ExcalidrawPlugin';
 import { INSERT_HORIZONTAL_RULE_COMMAND } from '../horizontal-rule/HorizontalRuleNode';
 import { INSERT_IMAGE_COMMAND } from '../image/ImagePlugin';
+import { INSERT_INLINE_IMAGE_COMMAND } from '../inline-image/InlineImagePlugin';
 import { INSERT_PAGE_BREAK } from '../page-break/PageBreakPlugin';
 import { $createStickyNode } from '../sticky-note/StickNote';
 
 export const InsertDropDown = () => {
   const [isInsertingImage, setIsInsertingImage] = useState(false);
   const [isImageUrlMode, setIsImageUrlMode] = useState(false);
-  const [isImageFileMode, setIsImageFileMode] = useState(true);
+  const [isImageFileMode, setIsImageFileMode] = useState(false);
+  const [isInlineImageInsertMode, setIsInlineImageInsertMode] = useState(true);
   const activeEditor = useAtomValue(activeEditorAtom);
   const insertItems = useMemo(() => {
     if (!activeEditor) return [];
@@ -41,10 +43,7 @@ export const InsertDropDown = () => {
         key: 'inline-image',
         label: 'Inline Image',
         icon: <span className='i-mdi:image-outline' un-text='xl!' />,
-        onClick: () => activeEditor.dispatchCommand(INSERT_IMAGE_COMMAND, {
-          src: 'https://picsum.photos/200/300',
-          altText: 'random image',
-        })
+        onClick: () => setIsInlineImageInsertMode(true),
       },
       {
         key: 'sticky-note',
@@ -130,7 +129,7 @@ export const InsertDropDown = () => {
     }
     {
       isImageFileMode &&
-      <Modal open={true} footer={null} onCancel={() => setIsImageFileMode(false)} title='Insert Image'>
+      <Modal open footer={null} onCancel={() => setIsImageFileMode(false)} title='Insert Image'>
         <Form un-mt='6' labelCol={{ span: 5 }} className='[&>div:last-child]:m-0'
           onFinish={values => {
             const { images: { fileList } } = values;
@@ -156,6 +155,53 @@ export const InsertDropDown = () => {
               <span className="i-material-symbols-light:upload" un-text='6xl blue-6' />
               <p>Click or drag file to this area to upload</p>
             </Upload.Dragger>
+          </Form.Item>
+          <Form.Item label={null} wrapperCol={{ offset: 20 }} >
+            <Button un-bg='blue-6' type='primary' htmlType='submit' >
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    }
+    {
+      isInlineImageInsertMode &&
+      <Modal open footer={null} onCancel={() => setIsInlineImageInsertMode(false)} title='Insert Inline Image'>
+        <Form un-mt='6' labelCol={{ span: 5 }} className='[&>div:last-child]:m-0'
+          onFinish={values => {
+            const { images: { fileList }, position, showCaption } = values;
+            if (Array.isArray(fileList)) {
+              activeEditor?.dispatchCommand(INSERT_INLINE_IMAGE_COMMAND, { src: URL.createObjectURL(fileList[0].originFileObj), altText: fileList[0].name, position, showCaption });
+            }
+            setIsInlineImageInsertMode(false);
+          }}
+        >
+          <Form.Item label='File' name='images' valuePropName='images' rules={[{ required: true, message: 'Please upload image!' }]} >
+            <Upload.Dragger listType='picture' accept='image/*' onPreview={() => {}} maxCount={1}
+              beforeUpload={() => false}
+              itemRender={(originalNode, file, _fileList) => {
+                return <div>
+                  {originalNode}
+                  <div un-flex='~' un-gap='1' mt='1' un-items='center' >
+                    Alt Text:
+                    <input un-flex-grow='1' un-border='rounded 2 solid gray-2' un-p='0.5' defaultValue={file.name} onChange={event => file.name = event.target.value} />
+                  </div>
+                </div>;
+              }}
+            >
+              <span className="i-material-symbols-light:upload" un-text='6xl blue-6' />
+              <p>Click or drag file to this area to upload</p>
+            </Upload.Dragger>
+          </Form.Item>
+          <Form.Item label='Position' name='position' >
+            <Radio.Group optionType='button' block defaultValue='left' >
+              <Radio value='left'>Left</Radio>
+              <Radio value='full'>Full Width</Radio>
+              <Radio value='right'>Right</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label='Show Caption' name='showCaption' valuePropName='checked' >
+            <Checkbox />
           </Form.Item>
           <Form.Item label={null} wrapperCol={{ offset: 20 }} >
             <Button un-bg='blue-6' type='primary' htmlType='submit' >

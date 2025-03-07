472650455f3e847da58033886aa9dcf5f80e65d5
Jiahua Zhang <hughzhanghz@gmail.com>
2025-03-07 02:29:43 -0500

    should fix vercel deployment issue

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,3 +1,4 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
-export default { ssr: true, } satisfies Config;
+export default { ssr: true, presets: [vercelPreset] } satisfies Config;

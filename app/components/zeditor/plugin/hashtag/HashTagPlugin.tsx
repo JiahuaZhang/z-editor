963c56c0feb291d77c1b8af7a83e8f94f411efa4
Jiahua Zhang <hughzhanghz@gmail.com>
2025-03-12 00:53:59 -0400

    update hash tag plugin

--- app/components/zeditor/plugin/hashtag/HashTagPlugin.tsx
+++ app/components/zeditor/plugin/hashtag/HashTagPlugin.tsx
@@ -1,13 +1,18 @@
 import { $createHashtagNode, HashtagNode } from '@lexical/hashtag';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { useLexicalTextEntity } from '@lexical/react/useLexicalTextEntity';
-import type { TextNode } from 'lexical';
-import { useCallback, useEffect } from 'react';
+import { atom, useSetAtom } from 'jotai';
+import { $getNodeByKey, type TextNode } from 'lexical';
+import { useCallback, useEffect, useRef } from 'react';
 
 const HASHTAG_REGEX = /(?<prefix>^|\s)(?<sign>#)(?<tag>[^#\s]+)/i;
 
+export const hashTagMapAtom = atom<Record<string, string>>({});
+
 export const HashtagPlugin = () => {
   const [editor] = useLexicalComposerContext();
+  const setHashTagMap = useSetAtom(hashTagMapAtom);
+  const hasInitalizedRef = useRef(false);
 
   useEffect(() => {
     if (!editor.hasNodes([HashtagNode])) {
@@ -28,11 +33,44 @@ export const HashtagPlugin = () => {
     return { start, end };
   }, []);
 
-  useLexicalTextEntity<HashtagNode>(
-    getHashtagMatch,
-    HashtagNode,
-    $createHashtagNode_,
-  );
+  useLexicalTextEntity<HashtagNode>(getHashtagMatch, HashtagNode, $createHashtagNode_,);
+
+  useEffect(() => editor.registerMutationListener(HashtagNode, (mutations) => {
+    editor.update(() => {
+      for (const [key, mutation] of mutations) {
+        const hashTagNode = $getNodeByKey(key) as HashtagNode;
+
+        if (mutation === 'created') {
+          setHashTagMap((prev) => ({ ...prev, [key]: hashTagNode.getTextContent() }));
+        } else if (mutation === 'destroyed') {
+          setHashTagMap((prev) => {
+            const newMap = { ...prev };
+            delete newMap[key];
+            return newMap;
+          });
+        } else if (mutation === 'updated') {
+          setHashTagMap((prev) => {
+            const newMap = { ...prev };
+            newMap[key] = hashTagNode.getTextContent();
+            return newMap;
+          });
+        }
+      }
+    });
+  }), [editor]);
+
+  useEffect(() => editor.registerUpdateListener(({ editorState }) => {
+    if (hasInitalizedRef.current) return;
+
+    console.log('initalized');
+    const hashTagNodes = Array.from(editorState._nodeMap.values())
+      .filter((node) => node instanceof HashtagNode) as HashtagNode[];
+
+    const newMap: Record<string, string> = {};
+    editor.read(() => hashTagNodes.forEach((node) => newMap[node.getKey()] = node.getTextContent()));
+    setHashTagMap(newMap);
+    hasInitalizedRef.current = true;
+  }), [editor]);
 
   return null;
 };

e12fffe7a2988c37d0ca2c021f14a9d4cc55f243
Jiahua Zhang <hughzhanghz@gmail.com>
2025-04-19 02:10:13 -0400

    update shared history context

--- app/components/zeditor/context/SharedHistoryContext.tsx
+++ app/components/zeditor/context/SharedHistoryContext.tsx
@@ -1,13 +1,19 @@
 import { HistoryState, createEmptyHistoryState } from '@lexical/history';
 import { createContext, useContext, useMemo } from 'react';
 
-type ContextShape = { historyState?: HistoryState; };
+type ContextShape = { historyState: HistoryState; };
 
-const Context = createContext<ContextShape>({});
+const Context = createContext<ContextShape | null>(null);
 
 export const SharedHistoryContext = ({ children }: { children: JSX.Element; }) => {
   const historyContext = useMemo(() => ({ historyState: createEmptyHistoryState() }), []);
   return <Context.Provider value={historyContext}>{children}</Context.Provider>;
 };
 
-export const useSharedHistoryContext = () => useContext(Context);
+export const useSharedHistoryContext = () => {
+  const historyContext = useContext(Context);
+  if (!historyContext) {
+    throw new Error('No history context found');
+  }
+  return historyContext;
+};

23f2a47c54087a216f912bcf029053e88003753b
Jiahua Zhang <hughzhanghz@gmail.com>
2025-04-21 02:53:50 -0400

    prepare for removing activeEditorAtom

--- app/components/zeditor/context/EditorContext.tsx
+++ app/components/zeditor/context/EditorContext.tsx
@@ -1,12 +1,9 @@
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { initialConfig } from '../config';
-import { TableContext } from '../plugin/table/TablePlugin';
 import { SharedHistoryContext } from './SharedHistoryContext';
 
 export const EditorContext = ({ children }: { children: JSX.Element; }) => <LexicalComposer initialConfig={initialConfig} >
   <SharedHistoryContext>
-    <TableContext>
-      {children}
-    </TableContext>
+    {children}
   </SharedHistoryContext>
 </LexicalComposer>;
--- app/components/zeditor/context/activeEditor.tsx
+++ app/components/zeditor/context/activeEditor.tsx
@@ -1,10 +1,12 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { atom, useSetAtom } from 'jotai';
 import { COMMAND_PRIORITY_CRITICAL, LexicalEditor, SELECTION_CHANGE_COMMAND } from 'lexical';
-import { useEffect } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
+// todo: this atom is shared
 export const activeEditorAtom = atom<LexicalEditor | null>(null);
 
+// mark this deprecated
 export const useActiveEditor = () => {
   const [editor] = useLexicalComposerContext();
   const setActiveEditor = useSetAtom(activeEditorAtom);
@@ -17,4 +19,22 @@ export const useActiveEditor = () => {
     },
     COMMAND_PRIORITY_CRITICAL
   ), [editor]);
+};
+
+const Context = createContext<LexicalEditor | null>(null);
+
+export const ActiveEditorContext = ({ children }: { children: JSX.Element; }) => {
+  const [editor] = useLexicalComposerContext();
+  const [activeEditor, setActiveEditor] = useState(editor);
+
+  useEffect(() => editor.registerCommand(
+    SELECTION_CHANGE_COMMAND,
+    (_payload, newEditor) => {
+      setActiveEditor(newEditor);
+      return false;
+    },
+    COMMAND_PRIORITY_CRITICAL
+  ), [editor]);
+
+  return <Context.Provider value={activeEditor}>{children}</Context.Provider>;
 };

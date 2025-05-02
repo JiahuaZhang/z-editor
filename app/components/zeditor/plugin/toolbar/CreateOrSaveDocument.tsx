f4659078ed41c8f04da0beab8f874b7d43aaa718
Jiahua Zhang <hughzhanghz@gmail.com>
2025-05-02 00:55:37 -0400

    init supabase

--- app/components/zeditor/plugin/toolbar/CreateOrSaveDocument.tsx
+++ app/components/zeditor/plugin/toolbar/CreateOrSaveDocument.tsx
@@ -1,14 +1,32 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { createClient } from '@supabase/supabase-js';
 import { Tooltip } from 'antd';
+import { useCommentContext } from '../comment/CommentContext';
+import { useHashTagContext } from '../hashtag/HashTagPlugin';
+import { useTimeNodeContext } from '../time/TimePlugin';
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
 
 export const CreateOrSaveDocument = () => {
   const [editor] = useLexicalComposerContext();
+  const { comments } = useCommentContext();
+  const hashTagMap = useHashTagContext();
+  const timeNodeMap = useTimeNodeContext();
 
-  // state:
-  // new document?
-  // old, need save? or alredy sync
-
-  return <button>
+  return <button onClick={async () => {
+    const content = editor.getEditorState().toJSON();
+    const tag = [...new Set(Object.values(hashTagMap))];
+    const reminder = Object.values(timeNodeMap)
+      .filter(node => node.getReminders().length > 0);
+    const result = {
+      content,
+      comment: comments,
+      tag,
+      reminder,
+    };
+    const response = await supabase.from('editor_documents').insert(result);
+    console.log(response);
+  }} >
     <Tooltip title='Create New Document' >
       <span className="i-mdi:create" un-text='xl green-6' />
     </Tooltip>

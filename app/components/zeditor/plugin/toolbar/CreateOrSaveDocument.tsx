02d63476bb572a4a979f6360bf5f695fe367be62
Jiahua Zhang <hughzhanghz@gmail.com>
2025-06-11 02:23:32 -0400

    add delete ui on dropdown

--- app/components/zeditor/plugin/toolbar/CreateOrSaveDocument.tsx
+++ app/components/zeditor/plugin/toolbar/CreateOrSaveDocument.tsx
@@ -1,5 +1,5 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { Tooltip } from 'antd';
+import { Dropdown, Tooltip } from 'antd';
 import { useCallback } from 'react';
 import { useFetcher } from 'react-router';
 import { useCommentContext } from '../comment/CommentContext';
@@ -24,31 +24,58 @@ export const CreateOrSaveDocument = () => {
       reminder,
     };
 
-    await fetcher.submit(document as any, {
-      method: 'post',
-      action: '/api/document/upsert',
-      encType: 'application/json'
-    });
+    // await fetcher.submit(document as any, {
+    //   method: 'post',
+    //   action: '/api/document/upsert',
+    //   encType: 'application/json'
+    // });
   }, [editor]);
+
+  // Stub for delete logic
+  const deleteDocument = useCallback(() => {
+    // TODO: Implement document deletion logic
+    // e.g., fetcher.submit({ id: ... }, { method: 'delete', action: '/api/document/delete' })
+    // For now, just alert
+    alert('Delete document (to be implemented)');
+  }, []);
 
   if (fetcher.state === 'submitting') {
     return <span className="i-ph:spinner" un-text='xl blue-3' un-cursor='pointer' un-animate='spin' />;
   }
 
   if (fetcher.state === 'idle') {
-    return <button onClick={upsertDocument} >
-      {
-        fetcher.data === undefined && <Tooltip title='Create New Document' >
-          <span className="i-mdi:create" un-text='xl green-6' />
-        </Tooltip>
-      }
-
-      {
-        fetcher.data?.error && <Tooltip title={`${fetcher.data?.error?.message}`} >
-          <span className="i-material-symbols-light:error" un-text='xl red-6' />
-        </Tooltip>
-      }
-    </button>;
+    return <div un-inline='grid' un-grid-auto-flow='col' un-items='center' un-gap='0.5'>
+      <Dropdown.Button className='[&>button:last-child]:w-5 [&>button:first-child]:(px-3)'
+        onClick={upsertDocument}
+        icon={<div un-grid='~'><span className="i-ph:caret-down" un-text='xl gray-4' un-w='4' /></div>}
+        menu={{
+          items: [
+            {
+              key: 'delete',
+              label: <button un-bg='white hover:red-4' un-text='hover:white' un-border='rounded' un-cursor='pointer'
+                un-flex='~' un-items='center' un-gap='1' un-px='2' un-py='1'
+                className='group'
+                onClick={deleteDocument} >
+                <span className="i-bi:trash3" un-text='xl red-4 group-hover:white' />
+                Delete
+              </button>,
+              onClick: deleteDocument,
+            },
+          ],
+        }}
+      >
+        {fetcher.data === undefined && (
+          <Tooltip title='Create New Document'>
+            <span className="i-mdi:create" un-text='xl green-6' />
+          </Tooltip>
+        )}
+        {fetcher.data?.error && (
+          <Tooltip title={`${fetcher.data?.error?.message}`}>
+            <span className="i-material-symbols-light:error" un-text='xl red-6' />
+          </Tooltip>
+        )}
+      </Dropdown.Button>
+    </div>;
   }
 
   // return <button>

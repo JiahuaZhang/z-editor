9cb1dddb7488f937b59d3bb15aaded7cfb395d82
Jiahua Zhang <hughzhanghz@gmail.com>
2025-06-26 01:25:19 -0400

    more detail log

--- app/routes/auth.$provider.tsx
+++ app/routes/auth.$provider.tsx
@@ -4,7 +4,10 @@ import { createSupabaseServerClient } from '~/util/supabase.server';
 const getPublicUrl = (request: Request) => {
   const forwardedHost = request.headers.get('x-forwarded-host');
   const forwardedProto = request.headers.get('x-forwarded-proto');
-  console.log('get public url', forwardedHost, forwardedProto, request.url);
+  console.log('Forwarded host', forwardedHost);
+  console.log('Forwarded proto', forwardedProto);
+  console.log('Request url', request.url);
+  console.log('Origin', new URL(request.url).origin);
   if (forwardedHost && forwardedProto) {
     return `${forwardedProto}://${forwardedHost}`;
   }
@@ -16,6 +19,7 @@ export const loader = () => redirect('/login');
 export const action: ActionFunction = async ({ request, params }) => {
   const { supabase, headers } = createSupabaseServerClient(request);
   const origin = getPublicUrl(request);
+  console.log('redirect to', `${origin}/auth/${params.provider}/callback`);
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: params.provider as any,
     options: {

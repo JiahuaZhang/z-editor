2696bd5f71929609d805c46ffb369eb61bf521d7
Jiahua Zhang <hughzhanghz@gmail.com>
2025-06-26 01:11:07 -0400

    test with server login

--- app/routes/auth.$provider.tsx
+++ app/routes/auth.$provider.tsx
@@ -1,14 +1,24 @@
 import { ActionFunction, redirect } from 'react-router';
 import { createSupabaseServerClient } from '~/util/supabase.server';
 
+const getPublicUrl = (request: Request) => {
+  const forwardedHost = request.headers.get('x-forwarded-host');
+  const forwardedProto = request.headers.get('x-forwarded-proto');
+  if (forwardedHost && forwardedProto) {
+    return `${forwardedProto}://${forwardedHost}`;
+  }
+  return new URL(request.url).origin;
+};
+
 export const loader = () => redirect('/login');
 
 export const action: ActionFunction = async ({ request, params }) => {
   const { supabase, headers } = createSupabaseServerClient(request);
+  const origin = getPublicUrl(request);
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: params.provider as any,
     options: {
-      redirectTo: `${new URL(request.url).origin}/auth/${params.provider}/callback`,
+      redirectTo: `${origin}/auth/${params.provider}/callback`,
     },
   });
 

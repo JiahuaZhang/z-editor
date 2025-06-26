faa3a9b9940a43d874d240f43303a02dca85fd1c
Jiahua Zhang <hughzhanghz@gmail.com>
2025-06-26 01:55:05 -0400

    rollback after config update

--- app/routes/auth.$provider.tsx
+++ app/routes/auth.$provider.tsx
@@ -1,29 +1,14 @@
 import { ActionFunction, redirect } from 'react-router';
 import { createSupabaseServerClient } from '~/util/supabase.server';
 
-const getPublicUrl = (request: Request) => {
-  const forwardedHost = request.headers.get('x-forwarded-host');
-  const forwardedProto = request.headers.get('x-forwarded-proto');
-  console.log('Forwarded host', forwardedHost);
-  console.log('Forwarded proto', forwardedProto);
-  console.log('Request url', request.url);
-  console.log('Origin', new URL(request.url).origin);
-  if (forwardedHost && forwardedProto) {
-    return `${forwardedProto}://${forwardedHost}`;
-  }
-  return new URL(request.url).origin;
-};
-
 export const loader = () => redirect('/login');
 
 export const action: ActionFunction = async ({ request, params }) => {
   const { supabase, headers } = createSupabaseServerClient(request);
-  const origin = getPublicUrl(request);
-  console.log('redirect to', `${origin}/auth/${params.provider}/callback`);
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: params.provider as any,
     options: {
-      redirectTo: `${origin}/auth/${params.provider}/callback`,
+      redirectTo: `${new URL(request.url).origin}/auth/${params.provider}/callback`,
     },
   });
 

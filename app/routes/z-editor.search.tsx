c351af29614f74c6c741da0dc5900749559ba4b6
Jiahua Zhang <hughzhanghz@gmail.com>
2025-06-27 01:00:40 -0400

    init search ui

--- app/routes/z-editor.search.tsx
+++ app/routes/z-editor.search.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunction, useLoaderData, useNavigate, useRouteLoaderData, type MetaFunction } from "react-router";
+import { ActionFunction, LoaderFunction, useFetcher, useLoaderData, useNavigate, useRouteLoaderData, type MetaFunction } from "react-router";
 import { ZEditorCard } from '~/components/zeditor/ZEditorCard';
 import { createSupabaseServerClient } from '~/util/supabase.server';
 import { Tables } from '~/util/supabase.type';
@@ -27,10 +27,15 @@ export const loader: LoaderFunction = async ({ request }) => {
   return { documents: data as Document[] };
 };
 
+export const action: ActionFunction = async ({ request, params }) => {
+  return 'good';
+};
+
 const Search = () => {
   const { documents, error } = useLoaderData<LoaderData>();
   const { user } = useRouteLoaderData('routes/z-editor');
   const navigate = useNavigate();
+  const fetcher = useFetcher();
 
   if (error) {
     return <div un-text="center" un-p="14">
@@ -48,7 +53,23 @@ const Search = () => {
 
   return (
     <div>
-      <h1 un-mt='2' un-text="2xl center" un-mb="4">Search Documents</h1>
+      {/* todo: search by # hashtag */}
+      {/* possibly elastic search alike */}
+      {/* filter by time range */}
+      <fetcher.Form method="post" un-shadow="g" un-m='2' un-mx='auto' un-grid='~' un-grid-flow='col' un-justify='center' un-gap='2'>
+        <input un-p='2' un-px='4' un-border='gray-2 1 solid rounded focus:blue-4' un-outline='none'
+          autoFocus
+          type="text"
+          name="search"
+          placeholder="Search..."
+        />
+        {/* todo: dropdown in future */}
+        <button un-bg='blue-4 hover:white' un-text='white hover:blue-4' un-p='2' un-px='4' un-border='rounded solid blue-4 2' un-cursor='pointer' un-shadow='sm'
+          type="submit"
+        >
+          OK
+        </button>
+      </fetcher.Form>
       <ul un-ml='4' un-grid='~' un-grid-flow='col' un-justify='start' un-gap='4'>
         {documents.map((doc) => (
           <li key={doc.id} >

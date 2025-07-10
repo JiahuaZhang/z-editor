91e2240725843d41fcf9216a8f552f6cfcfe9640
Jiahua Zhang <hughzhanghz@gmail.com>
2025-07-10 01:23:27 -0400

    resolve devtool issue

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { reactRouter } from '@react-router/dev/vite';
 import path from 'path';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    devtoolsJson(),
     UnoCSS(),
     reactRouter({
       ignoredRouteFiles: ['**/.*']

21682c22af9cd47719c4c1c1920fd0cb05994688
Jiahua Zhang <hughzhanghz@gmail.com>
2025-08-15 12:29:21 -0400

    fix server issue

--- app/components/UserPanel.tsx
+++ app/components/UserPanel.tsx
@@ -1,5 +1,6 @@
 import type { User } from '@supabase/supabase-js';
 import { Avatar, Popover } from 'antd';
+import { Suspense } from 'react';
 import { Link, NavLink } from 'react-router';
 
 const getInitials = (name?: string, email?: string) => {
@@ -28,38 +29,49 @@ export const UserPanel = ({ user }: { user: User; }) => {
   const userEmail = user.email;
 
   return (
-    <div un-flex='~' un-gap='2' >
+    <div un-flex='~' un-gap='2' un-mb='1' >
       <div un-w='full' un-flex='~' un-items='center' >
         <NavLink to="/z-editor/search" prefetch='render'>
-          {({ isActive }) => <div un-flex='~' un-text={`${isActive && 'blue-400'} hover:blue-400`} un-gap='0.5' un-mx='2'>
+          {({ isActive }) => <div un-flex='~' un-text={`${isActive && 'blue-400'} hover:blue-400`} un-gap='0.5' un-p='2'
+            un-shadow='hover:[0_2px_2px_#51a2ff]'
+            un-border='rounded'
+          >
             <span className="i-material-symbols-light:search" un-text='2xl' />
             Search
           </div>}
         </NavLink>
         <NavLink to="/z-editor/alert" prefetch='render'>
-          {({ isActive }) => <div un-flex='~' un-text={`${isActive && 'blue-400'} hover:blue-400`} un-gap='0.5' un-px='2'>
+          {({ isActive }) => <div un-flex='~' un-text={`${isActive && 'blue-400'} hover:blue-400`} un-gap='0.5' un-p='2'
+            un-shadow='hover:[0_2px_2px_#51a2ff]'
+            un-border='rounded'
+          >
             <span className="i-material-symbols-light:notifications" un-text='2xl' />
             Alert
           </div>}
         </NavLink>
         <NavLink to="/z-editor/new" prefetch='render'>
-          {({ isActive }) => <div un-flex='~' un-text={`${isActive && 'blue-400'} hover:blue-400`} un-gap='0.5' un-px='2'>
+          {({ isActive }) => <div un-flex='~' un-text={`${isActive && 'blue-400'} hover:blue-400`} un-gap='0.5' un-p='2'
+            un-shadow='hover:[0_2px_2px_#51a2ff]'
+            un-border='rounded'
+          >
             <div className="i-material-symbols-light:add-circle-outline" un-text='2xl' />
             New
           </div>}
         </NavLink>
       </div>
-      <Popover content={logoutContent} trigger="click">
-        <div un-cursor="pointer" >
-          {avatarUrl ? (
-            <Avatar src={avatarUrl} alt={user.user_metadata.full_name} size='large' />
-          ) : (
-            <Avatar un-bg='blue-400' size='large' >
-              {getInitials(userName, userEmail)}
-            </Avatar>
-          )}
-        </div>
-      </Popover>
+      <Suspense>
+        <Popover content={logoutContent} trigger="click">
+          <div un-cursor="pointer" >
+            {avatarUrl ? (
+              <Avatar src={avatarUrl} alt={user.user_metadata.full_name} size='large' />
+            ) : (
+              <Avatar un-bg='blue-400' size='large' >
+                {getInitials(userName, userEmail)}
+              </Avatar>
+            )}
+          </div>
+        </Popover>
+      </Suspense>
     </div>
   );
 };
